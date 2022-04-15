807045972a574be644a9315443fdcf27fa783a13
Mark gottselig <grevenilvec@hotmail.com>
2022-04-15 15:45:27 -0600

    Add source code generator Add code project for marker attribute Restructure files to better support multiple projects

---
+++ test/Chatter.Rest.Hal.CodeGenerators.Tests/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Chatter.Rest.Hal.CodeGenerators.Tests;
+
+[HalResponse]
+public partial class Person
+{
+	public int Age { get; set; } = default!;
+	public string[] Friends { get; set; } = default!;
+	public string Name { get; set; } = default!;
+}
