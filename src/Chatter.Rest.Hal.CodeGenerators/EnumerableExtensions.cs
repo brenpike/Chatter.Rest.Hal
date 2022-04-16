70a214e52a76a839ed24db2648ba127c32d67d6b
Mark gottselig <grevenilvec@hotmail.com>
2022-04-16 00:59:42 -0600

    Refactor code generator Add extra test for scoped namespaces

---
+++ src/Chatter.Rest.Hal.CodeGenerators/EnumerableExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Chatter.Rest.Hal.CodeGenerators;
+
+public static class EnumerableExtensions
+{
+	public static IEnumerable<T> NotNull<T>(this IEnumerable<T?> enumerable) where T : class =>
+		enumerable.Where(static e => e != null).Select(static e => e!);
+}
