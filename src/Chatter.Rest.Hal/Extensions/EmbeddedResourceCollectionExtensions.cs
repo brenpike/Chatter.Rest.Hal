1f139500ddc08a180345823c96eb0e2eb7312e75
Brennan Pike <brenpike@gmail.com>
2022-12-16 10:31:00 -0700

    add extension methods

---
+++ src/Chatter.Rest.Hal/Extensions/EmbeddedResourceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Chatter.Rest.Hal.Extensions;
+
+public static class EmbeddedResourceCollectionExtensions
+{
+	public static IEnumerable<Resource> GetResourcesByName(this EmbeddedResourceCollection erc, string name) 
+		=> erc.Where(er => er.Name.Equals(name)).SelectMany(r => r.Resources);
+}
